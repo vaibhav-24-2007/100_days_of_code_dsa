950407729bc3d89ec622151961529e6e01013f79
vaibhav-24-2007 <vaimah2007@gmail.com>
2026-02-16 23:06:29 +0530

    Create day16_q1.c

---
+++ day16_q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, count;
+
+    printf("enter no. of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int visited[n];
+
+    printf("enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(visited[i] == 1)
+            continue;
+
+        count = 1;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d occurs %d times\n", arr[i], count);
+    }
+
+    return 0;
+}
