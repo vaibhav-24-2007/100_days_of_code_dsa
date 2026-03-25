4a1a3a7706a638ff0946de114bd5c43f92cebe71
vaibhav-24-2007 <vaimah2007@gmail.com>
2026-03-25 09:40:00 +0530

    Create day43_q2.c

---
+++ day43_q2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+// create node
+struct node* newNode(int x) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// max depth function
+int maxDepth(struct node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    if (left > right)
+        return left + 1;
+    else
+        return right + 1;
+}
+
+// example
+int main() {
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    printf("%d\n", maxDepth(root));
+
+    return 0;
+}
